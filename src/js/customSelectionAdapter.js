1db160501f4cea442ed3090e2ee9c769461b76d3
gareth-ib <gareth.pursehouse.ib@gmail.com>
2019-08-07 17:19:05 -0700

    change to set "select2-selection--empty" class on empty object

--- src/js/customSelectionAdapter.js
+++ src/js/customSelectionAdapter.js
@@ -34,7 +34,7 @@ $.fn.select2.amd.define('select2/selection/customSelectionAdapter',
 
       this.$selectionTagsContainer.html(this.$selectionTags);
 
-      this.$selectionTags.hide();
+      this.$selectionTagsContainer.addClass('select2-selection--empty');
 
       if (this.options.get('selectionContainer')) {
         this.$selectionTagsContainer.appendTo(this.options.get('selectionContainer'));
@@ -87,7 +87,7 @@ $.fn.select2.amd.define('select2/selection/customSelectionAdapter',
     };
 
     CustomSelection.prototype.clear = function () {
-      this.$selectionTags.hide();
+      this.$selectionTagsContainer.addClass('select2-selection--empty');
       // we need this line to remove the existing .select2-selection__clear button
       this.$selection.find('.select2-selection__rendered').empty();
       this.$selectionTags.find('.select2-selection__rendered').empty();
@@ -118,6 +118,8 @@ $.fn.select2.amd.define('select2/selection/customSelectionAdapter',
       if (data.length === 0) {
         return;
       }
+      
+      this.$selectionTagsContainer.removeClass('select2-selection--empty');
 
       this.$selectionTags.show();
 
